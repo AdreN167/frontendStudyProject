03635248e8ca78fbb2fc7c9ba0a79677fd44745f
AdreN167 <zz11926557@gmail.com>
2024-02-01 17:54:37 +0700

    add js for navigation and pp-form

---
+++ src/scripts/dev/main.js
@@ -0,0 +1,60 @@
+(function () {
+  "use strict";
+
+  const root = document.documentElement;
+  const navToggle = root.querySelector("#js-navToggle");
+
+  navToggle.addEventListener("click", function () {
+    root.classList.toggle("show-nav");
+  });
+
+  const eventPP = root.querySelector("#js-eventPP");
+  const eventOpenBtn = root.querySelector("#js-eventOpenBtn");
+
+  if (eventPP && eventOpenBtn) {
+    const closeEventPP = function (event) {
+      function close() {
+        document.removeEventListener("keyup", closeEventPP);
+        eventPP.removeEventListener("click", closeEventPP);
+        root.classList.remove("show-event-popup");
+      }
+
+      switch (event.type) {
+        case "keyup":
+          if (event.key === "Escape" || event.keyCode === 27) close();
+          break;
+        case "click":
+          if (
+            event.target === this ||
+            event.target.classList.contains("js-eventCloseBtn")
+          )
+            close();
+          break;
+      }
+    };
+
+    eventOpenBtn.addEventListener("click", function() {
+        root.classList.add("show-event-popup");
+
+        document.addEventListener("keyup", closeEventPP);
+        eventPP.addEventListener("click", closeEventPP);
+    })
+
+    //     eventOpenBtn.addEventListener("click", function () {
+    //       root.classList.add("show-event-popup");
+    //     });
+
+    //     eventPP.addEventListener("click", function(event) {
+    //         if (event.target === this || event.target.classList.contains("js-eventCloseBtn")) {
+    //             root.classList.remove("show-event-popup");
+    //         }
+    //     });
+    //   }
+
+    //   document.addEventListener("keyup", function(event) {
+    //     if (event.key === "Escape" || event.keyCode === 27) {
+    //         root.classList.remove("show-event-popup");
+    //     }
+    //   })
+  }
+})();
